de46b25babd1ada43313d1babb52591a792675f8
vasavi <vasavi4083547@gmail.com>
2020-08-25 16:04:31 +0530

    1st commit

---
+++ src/resumeData.js
@@ -0,0 +1,42 @@
+let resumeData = {
+    "imagebaseurl":"https://techonoeticsclub.github.io/",
+    "name": "Vasavi s",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, or simply read in my free time",
+    "socialLinks":[
+      
+        {
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am a full-time student and a part-time developer.",
+    "address":"India",
+    "mail":"webamteur0707@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Manipal Institute of Technology",
+        "specialization":"Electrical and Eloctronics Engineering",
+         },
+      
+    ],
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
